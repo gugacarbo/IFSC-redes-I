0462d20428f12caec0826c7b4e7b2d43f9a6ddfb
alunolages <alunolages@lages.sc>
2026-05-12 09:44:41 -0300

    java format

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,6 @@ export default {
 	"*.{ts,tsx,js,jsx,json}": "biome check --write",
 	"*.md": "prettier --write",
 	"apps/**/src/**/*.java": [
-		(files) => `node ../../tools/java/checkstyle-runner.cjs ${files.join(" ")}`,
+		(files) => `node ./tools/java/checkstyle-runner.cjs ${files.join(" ")}`,
 	],
 };
