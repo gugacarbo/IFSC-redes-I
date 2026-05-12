1bf0b388e335bdc4eab4f651c4fde8415b3bbcd1
Gustavo <guga_carbo@hotmail.com>
2026-05-12 20:51:50 -0300

    feat(filial): add wifi connection logic

--- apps/filial-esp32/src/main.cpp
+++ apps/filial-esp32/src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
+#include <WiFi.h>
 #include "ConfigManager.h"
 #include "DeviceManager.h"
 
+// Hardcode for testing environment
+const char* WIFI_SSID = "WOKWI-GUEST"; // Typical test/simulator SSID
+const char* WIFI_PASS = "";
+
 FilialConfig globalConfig;
 DeviceManager deviceManager;
+
+void connectWiFi() {
+  Serial.printf("Connecting to %s ", WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" CONNECTED");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
 
 void setup() {
   Serial.begin(115200);
@@ -14,6 +31,8 @@ void setup() {
           Serial.println("Devices initialized.");
       }
   }
+  
+  connectWiFi();
 }
 
 void loop() {}
