90ee307f6fbdf9e611f44067c78bb41673d7a171
Gustavo <guga_carbo@hotmail.com>
2026-05-13 00:30:16 -0300

    save

--- apps/udp-iot/applications/matriz-gui/vite.config.ts
+++ apps/udp-iot/applications/matriz-gui/vite.config.ts
@@ -1,23 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
-
-import { loadEnv } from 'vite'
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: { "@": path.resolve(__dirname, "./src") }
-  },
-  build: {
-    outDir: '../matriz-esp32/data/www',
-    emptyOutDir: true
-  }
-    server: {
-      port: parseInt(env.VITE_MATRIZ_PORT || '5173', 10),
-    }
-  }
-})
+	const env = loadEnv(mode, process.cwd(), "");
+	return {
+		plugins: [react(), tailwindcss()],
+		resolve: {
+			alias: { "@": path.resolve(__dirname, "./src") },
+		},
+		build: {
+			outDir: "../matriz-esp32/data/www",
+			emptyOutDir: true,
+		},
+		server: {
+			port: parseInt(env.VITE_MATRIZ_PORT || "5173", 10),
+		},
+	};
+});
